9bfdc53567da131416a77258964caebaea8a1813
Media Administrator - C-EXINS.CO <80618157+c-exinsco@users.noreply.github.com>
2022-06-04 20:50:01 +0500

    C-EXINS blockchain department, Odeonus.com.
    Name: CXTron
    Symbol: CXT
    Contract Address: TM39aqx78oWCNDrWxqwFhz18bLC4kW7yQA
    Decimals: 6
    
    CXTron CXT Token. Created/Owned by C-EXINS. Launched today on
    28 of May, 2022.

---
+++ CXTron.sol
@@ -0,0 +1,24 @@
+// 0.5.1-c8a2 - NOT
+// Enable optimization
+// pragma solidity ^0.5.0 - NOT;
+// C-EXINS CXTron.sol Tron TRC20 Token
+pragma solidity ^0.5.1;
+
+import "./TRC20.sol";
+import "./TRC20Detailed.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `TRC20` functions.
+ */
+contract Token is TRC20, TRC20Detailed {
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public TRC20Detailed("CXTron", "CXT", 6) {
+        _mint(msg.sender, 1000000000000 * (10 ** uint256(decimals())));
+    }
+}
